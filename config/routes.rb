890b6c9f905cdbd04b55fdfd4d41fe82e7cd2a93
Ubuntu <ubuntu@ip-172-31-14-167.ap-northeast-1.compute.internal>
2022-06-30 09:33:15 +0000

    [Add]名前付きルートの追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
   root 'static_pages#top'
-  get 'static_pages/top'
-  get 'static_pages/terms'
-  get 'static_pages/privacy_policy'
-  get 'static_pages/inquiry'
-  get 'static_pages/introduction'
+  get  '/terms',    to: 'static_pages#terms'
+  get  '/privacy_policy',    to: 'static_pages#privacy_policy'
+  get  '/inquiry',    to: 'static_pages#inquiry'
+  get  '/introduction',    to: 'static_pages#introduction'
 end
