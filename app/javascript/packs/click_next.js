7b1a4ba23926ea610a059c48767b12fcf67979ea
Ubuntu <ubuntu@ip-172-31-14-167.ap-northeast-1.compute.internal>
2022-07-08 05:03:33 +0000

    [Add]タイプアニメーションの追加

--- app/javascript/packs/click_next.js
+++ app/javascript/packs/click_next.js
@@ -1,16 +1,20 @@
 /* global $ */
 
+import {typing} from './typing.js';
+
 $(document).on('turbolinks:load', function() {
-  var num = 0;
+  let num = 0;
 $(".next2").click(function(){
   $(this).data("click", ++num)
-  var click = $(this).data("click");
+  let click = $(this).data("click");
   if(click == 1){
     document.getElementById('a').textContent = '＊ 色は匂へど 散りぬるを 我が世誰そ 常ならむ'
     document.getElementById('b').textContent = 'いろはにおへど ちりぬるを わがよたれそ つねならむ'
+    typing();
   } else if(click == 2){
     document.getElementById('a').textContent = '＊ 有為の奥山 今日越えて'
     document.getElementById('b').textContent = '浅き夢見じ 酔ひもせず'
+    typing();
   }　else if(click == 3){
     $(this).attr("href", "/")
   }else{
--- app/javascript/packs/typing.js
+++ app/javascript/packs/typing.js
@@ -1,57 +1,15 @@
 /* global $ */
-// /* ----- option ----- */
-// var id = ['a','b']; //指定するidを全て配列で渡す
-// var txSp = 50; // テキストの表示速度
-// var dly = 1000; // 次の文章までの待ち時間
-// /* ----- option ----- */
-// var count = 0;
-// var tx = [];
-// var txCount = [];
-
-// window.onload = function(){
-//   kamikakushi();
-//   countSet();
-//   itimozi();
-// }
-
-// function countSet(){ // 文字数カウントの初期設定
-//   for(let n=0;n<id.length;n++){
-//     txCount[n] = 0;
-//   }
-// }
-
-// function kamikakushi(){ // 要素をtx[i]に保持させ、非表示にする
-//   for(let i=0;i<id.length;i++){
-//     id[i] = document.getElementById(id[i]);
-//     tx[i] = id[i].firstChild.nodeValue; // 初期の文字列
-//     id[i].innerHTML = '';
-//   }
-// }
-
-// function itimozi(){//　一文字ずつ表示させる
-//   id[count].innerHTML = tx[count].substr( 0, ++txCount[count] ); // テキストの指定した数の間の要素を表示
-//   if(tx[count].length != txCount[count]){ // Count が初期の文字列の文字数と同じになるまでループ
-//     setTimeout(itimozi,txSp); // 次の文字へ進む
-//     // alert(`${tx[count].length}番と${txCount[count]}番`)
-//   }else{
-//   id[count].innerHTML = tx[count].substr( 0, ++txCount[count] ); // テキストの指定した数の間の要素を表示
-//     count++; // 次の段落に進む為のカウントアップ
-//     if(count != id.length){ // id数が最後なら終了
-//     setTimeout(itimozi,dly); // 次の段落へ進む
-//     }
-//   }
-// }
 
 
-$(document).on('turbolinks:load', function() {
+function typing() {
     /* ----- option ----- */
-    var id = ['a','b']; //指定するidを全て配列で渡す
-    var txSp = 50; // テキストの表示速度
-    var dly = 1000; // 次の文章までの待ち時間
+    let id = ['a','b']; //指定するidを全て配列で渡す
+    let txSp = 40; // テキストの表示速度
+    let dly = 1000; // 次の文章までの待ち時間
     /* ----- option ----- */
-    var count = 0;
-    var tx = [];
-    var txCount = [];
+    let count = 0;
+    let tx = [];
+    let txCount = [];
     
     kamikakushi();
     countSet();
@@ -85,5 +43,21 @@ $(document).on('turbolinks:load', function() {
         }
     }
     
-    
+}
+
+
+
+
+$(document).on('turbolinks:load', function(){
+    if(document.URL.match(/introduction/)){
+    typing()
+    }
 })
+
+
+
+
+
+
+
+export{typing};
