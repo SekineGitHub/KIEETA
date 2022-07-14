68a16f2a5186ede59b8eec2455c67527cf01d502
8901 <kagurachiwawa@icloud.com>
2022-07-14 09:17:37 +0000

    [Add]写真の複数表示（仮）

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -6,6 +6,7 @@ class ListsController < ApplicationController
   
   def show
     @list = List.find(params[:id])
+    @photos = @list.photos
   end
   
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,21 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+List.create!(name: "小暮洋史（こぐれ ひろし）",
+             age: "５２歳",
+             height: "１７０センチ位",
+             guilty: "殺人",
+             bounty: "３００万円",
+             story: "職場で出会った女性に一目惚れをし、ストーカー行為を繰り返す。大雪のふるある日、女性宅に侵入。同居していた父、母、祖母を皆殺しにする。その後、帰宅した女性を襲うが、必死の抵抗と説得により、小暮は姿を消す。",
+             report: "https://www.police.pref.gunma.jp/keijibu/02sou1/koguret.html"
+)
+
+Photo.create!(
+  [
+    {
+      list_id: 1,
+      image: "https://lh3.googleusercontent.com/pw/AM-JKLVuS2nEkSWhC0s4YrBhwj-QUCUiAPanZX1tzTqBnYi7tTjgVf1sjlFys_np_vJUrKWxLE6PtWGReNfxrVoEoUlDJht05aQPDC5rnJww64tbqAWCrMVDFL6cI6DR_STl0wsTz0XJd7S3q2reWQgt3Aw=w149-h224-no?authuser=0"
+    },
+    {
+      list_id: 1,
+      image: "https://lh3.googleusercontent.com/pw/AM-JKLX6qTst-qaQHHZNIsc0VIxAgM3zRIKptqYQ245nW1YMrNobDF-pJqGgBE-xvP4duCswN1hpZ6mIvYa0uBoVfRR07G1R9oYcG8ndP9-cFaLYHv8Tm8fm0QP3sp08hV8X7Cqq20CCwOjVPUfy0WReYFo=w158-h352-no?authuser=0"
+    }
+  ]
+)
