a33308d8582393ec986f75aa2b8400329cd2a0d3
8901 <kagurachiwawa@icloud.com>
2022-07-16 09:47:35 +0000

    [Add]人物seedデータの入力

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,125 @@
-List.create!(name: "小暮洋史（こぐれ ひろし）",
-             age: "５２歳",
-             height: "１７０センチ位",
-             guilty: "殺人",
-             bounty: "３００万円",
-             story: "職場で出会った女性に一目惚れをし、ストーカー行為を繰り返す。大雪のふるある日、女性宅に侵入。同居していた父、母、祖母を皆殺しにする。その後、帰宅した女性を襲うが、必死の抵抗と説得により、小暮は姿を消す。",
-             report: "https://www.police.pref.gunma.jp/keijibu/02sou1/koguret.html"
+List.create!(
+  [
+    {
+      name: "小暮洋史（こぐれ ひろし）",
+      age: "５２歳",
+      height: "１７０センチ位",
+      guilty: "殺人",
+      bounty: "３００万円",
+      story: "職場で出会った女性に一目惚れをし、ストーカー行為を繰り返す。大雪のふるある日、女性宅に侵入。同居していた父、母、祖母を皆殺しにする。その後、帰宅した女性を襲うが、必死の抵抗と説得により、小暮は姿を消す。",
+      report: "https://www.police.pref.gunma.jp/keijibu/02sou1/koguret.html"
+    },
+    {
+      name: "小原勝幸（おばら かつゆき）",
+      age: "４１歳",
+      height: "１７０センチ",
+      guilty: "殺人",
+      bounty: "３００万円",
+      story: "仮のテキスト",
+      report: "https://www.pref.iwate.jp/kenkei/anzen/joho/sousa/3000346.html"
+    },
+    {
+      name: "上地恵栄（うえち けいえい）",
+      age: "６５歳",
+      height: "１６８センチ位",
+      guilty: "殺人",
+      bounty: "３００万円",
+      story: "仮のテキスト",
+      report: "https://www.keishicho.metro.tokyo.lg.jp/jiken_jiko/ichiran/ichiran_11-20/mitaka2.html"
+    },
+    {
+      name: "見立真一（みたて しんいち）",
+      age: "４２歳",
+      height: "１６７センチ位",
+      guilty: "殺人、凶器準備集合",
+      bounty: "６００万円",
+      story: "仮のテキスト",
+      report: "https://www.keishicho.metro.tokyo.lg.jp/jiken_jiko/ichiran/ichiran_24/azabu3.html"
+    },
+    {
+      name: "リン ショウイ",
+      age: "４８歳",
+      height: "１７８センチ位",
+      guilty: "強盗殺人",
+      bounty: "０円",
+      story: "仮のテキスト",
+      report: "https://www.pref.aichi.jp/police/anzen/sousa/shimei/minami/minami-199910.html"
+    },
+    {
+      name: "吉屋強（よしや つよし）",
+      age: "４４歳",
+      height: "１７２センチ位",
+      guilty: "殺人",
+      bounty: "０円",
+      story: "仮のテキスト",
+      report: "https://www.keishicho.metro.tokyo.lg.jp/jiken_jiko/ichiran/ichiran_11-20/yoshiya.html"
+    },
+    {
+      name: "宮内雄大（みやうち ゆうだい）",
+      age: "４６歳",
+      height: "１７７センチ位",
+      guilty: "強盗致傷",
+      bounty: "１００万円",
+      story: "仮のテキスト",
+      report: "https://www.pref.yamanashi.jp/police/p_keiki/p_keiki/shimeitehai.html"
+    },
+    {
+      name: "リ カイ",
+      age: "５７歳",
+      height: "１６５センチ位",
+      guilty: "強盗殺人",
+      bounty: "０円",
+      story: "仮のテキスト",
+      report: "https://www.keishicho.metro.tokyo.lg.jp/jiken_jiko/ichiran/ichiran_11-20/likai.html"
+    },
+    {
+      name: "越智 清（おち きよし）",
+      age: "６９歳",
+      height: "１７５センチ位",
+      guilty: "殺人",
+      bounty: "０円",
+      story: "仮のテキスト",
+      report: "https://www.police.pref.osaka.lg.jp/jiken/wanted/3622.html"
+    },
+    {
+      name: "又吉建男（またよし たてお）",
+      age: "７２歳",
+      height: "１７２センチ位",
+      guilty: "殺人",
+      bounty: "０円",
+      story: "仮のテキスト",
+      report: "https://www.police.pref.okinawa.jp/docs/2015031700049/"
+    },
+    {
+      name: "桐島聡（きりしま さとし）",
+      age: "６７歳",
+      height: "１６０センチ位",
+      guilty: "爆発物取締罰則違反",
+      bounty: "０円",
+      story: "仮のテキスト",
+      report: "https://www.keishicho.metro.tokyo.lg.jp/jiken_jiko/ichiran/ichiran_10/kirishima.html"
+    },
+    {
+      name: "菱川龍己（ひしかわ たつみ）",
+      age: "４５歳",
+      height: "１５８センチ位",
+      guilty: "殺人、銃刀法違反",
+      bounty: "０円",
+      story: "仮のテキスト",
+      report: "https://www.police.pref.hyogo.lg.jp/teikyo/sosa/index.htm"
+    },
+    {
+      name: "金成行（きん しげゆき）",
+      age: "５３歳",
+      height: "１７０センチ位",
+      guilty: "殺人未遂",
+      bounty: "０円",
+      story: "仮のテキスト",
+      report: "https://www.pref.nagano.lg.jp/police/sousa/hansou/miyadatehai.html"
+    }
+  ]
 )
+
 
 Photo.create!(
   [
