e06040c6dc8e8536f4a07a4354aab8d42b39005b
8901 <kagurachiwawa@icloud.com>
2022-07-17 05:36:08 +0000

    [Add]写真seedデータ追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -121,15 +121,237 @@ List.create!(
 )
 
 
+
 Photo.create!(
   [
-    {
+    # 小暮洋史（こぐれ ひろし）
+    { 
       list_id: 1,
       image: "https://lh3.googleusercontent.com/pw/AM-JKLVuS2nEkSWhC0s4YrBhwj-QUCUiAPanZX1tzTqBnYi7tTjgVf1sjlFys_np_vJUrKWxLE6PtWGReNfxrVoEoUlDJht05aQPDC5rnJww64tbqAWCrMVDFL6cI6DR_STl0wsTz0XJd7S3q2reWQgt3Aw=w149-h224-no?authuser=0"
     },
     {
       list_id: 1,
       image: "https://lh3.googleusercontent.com/pw/AM-JKLX6qTst-qaQHHZNIsc0VIxAgM3zRIKptqYQ245nW1YMrNobDF-pJqGgBE-xvP4duCswN1hpZ6mIvYa0uBoVfRR07G1R9oYcG8ndP9-cFaLYHv8Tm8fm0QP3sp08hV8X7Cqq20CCwOjVPUfy0WReYFo=w158-h352-no?authuser=0"
+    },
+    {
+      list_id: 1,
+      image: "https://lh3.googleusercontent.com/pw/AM-JKLV6OO25mjYBNcHd_J8Y8vJyyAx3ZSv_5SqgQSwxdnxT1LXH3--yX4fRrR9FxVituqR_gy3U94e0Tq4MgHURvPAB1eK7dN8twPQpNU7pSvE1yowMoRRbd9EwVHNmUv2qqqVz3o6ApSePIkamovtGiPM=w841-h1190-no?authuser=0"
+    },
+    {
+      list_id: 1,
+      image: "https://lh3.googleusercontent.com/pw/AM-JKLVp-okZViwDTBASbdTF3JuVu_weN9nKF6Xvs0pLlt_tf_cMHWrYqt5xL1qc_PBrTu7vlLOwb4-Q-8zQt2877YeujPjT84VxW8rkw8aj8VQy9Mg5wjmIg_TMdJqw3XhKX5NezX3VFkYOdP9lrvy8yN4=w583-h467-no?authuser=0"
+    },
+    {
+      list_id: 1,
+      image: "https://lh3.googleusercontent.com/pw/AM-JKLWWbcgcOCfC8ht9NnIzeohON9LXUl25eFhMnoUFUIA91Cvtb7ZfeSDqxFdS2852pWLwcgzPY4iZaZM5vPBuQ0LIw2tJVa-wqVekgVKMLL9kOqvUCqdc2uBY4yg2VMXiDmbSMml5x465udaq7DLxsXI=w587-h470-no?authuser=0"
+    },
+    {
+      list_id: 1,
+      image: "https://lh3.googleusercontent.com/pw/AM-JKLV9EnsUUaX7LU-UTIbRyZ9E_Seb9ZuZjJ36YSal7WhUwFaA57J2CysMYvySl2e-Hrzwif6zwm69p_Xa5lmE34U-sfSfaPmjJOHaudixpsnDReusMwvGo7NsyXtJU5gGaum_dl13RSSAXxThXD8BFO0=w588-h461-no?authuser=0"
+    },
+    {
+      list_id: 1,
+      image: "https://lh3.googleusercontent.com/pw/AM-JKLUvV2eGQ7G1BSD0DX9_KSUJQsLBlB9Mye1wH7haZCadxjNxEBQgYnasxfkqatb884UxXnLotPfoUoHq92RcMPVV3cc-wBVNrP5Tx41ROzveP4cEQXi2pWM6sk95I1uA4w7m3UlSZHHjYlauihplUUE=w593-h470-no?authuser=0"
+    },
+    {
+      list_id: 1,
+      image: "https://lh3.googleusercontent.com/pw/AM-JKLXeQipyOFMv1n7q2h9Jd6vwMudceqBIbgd6rFrykRiBpQ4i32I0gKAbQoYAOSKDb5akgjl3dVYgGCujRli58nPX4WYRa9jB5Gxangzz38IIP-52FiwwvmWsGtyoWcuk4PlpFb79DM2N9Z0Gv9vY2Bc=w592-h472-no?authuser=0"
+    },
+    {
+      list_id: 1,
+      image: "https://lh3.googleusercontent.com/pw/AM-JKLWeJNFpYCUy4MkzkarDhAF8qgVVWgvzn12PCCIHRc7IiPKXkjyf-LaBbadqG2P9R-DMfKHbHH0h064QiveCF9j7LDRQ7agYF_sVnybw149hjqZaS8XrkzvzcHjlv_Pl0cWPqN8VKtcQoTZBt9M8pkc=w592-h474-no?authuser=0"
+    },
+    {
+      list_id: 1,
+      image: "https://lh3.googleusercontent.com/pw/AM-JKLUn2mX4PNYOwino8MAetHsAbX4saYON_6ksd-QsGHAzyOt1Yj6bIicWktcKDjkghh9s8Tqfv6m9wkBg9MAi7rrmmMgGqXvns0PZs-ekAK97kcLt6_83PKKP3C04wm8oNSVPGd-X-cb8gU6UCuySkNI=w589-h472-no?authuser=0"
+    },
+    {
+      list_id: 1,
+      image: "https://lh3.googleusercontent.com/pw/AM-JKLWDVTKWd1Z_lQdGo91ccN22P1r5R-MRWeunUKVRuSdKzcYityl7hHS8inIkofno8xhTl11-zNclAVMorugpnALfrFt9P_DdSHfS5nDRL94aTAxNy-Hc305H3CITJkaOG15giJlV9LyDZ6fKEguzGM8=w594-h463-no?authuser=0"
+    },
+    {
+      list_id: 1,
+      image: "https://lh3.googleusercontent.com/pw/AM-JKLVOr8iVVS8-UwUnYg1e3kIlxAzVmJiaNkRxIexFDbp-lyej16T1sq8oX0qgsMiodDC3hmI3H9hboNFw1jnM6jy_HHUBqd6UuxuBj9zA_msoRKdvutCF2LMzFagzWY1Ex2Z3NHDMq3bETSAP9ZZ_6qA=w592-h469-no?authuser=0"
+    },
+    {
+      list_id: 1,
+      image: "https://lh3.googleusercontent.com/pw/AM-JKLVjzfBdQ-Ai1d6cEyi6-DPplkvVXHONk6bJxR3--4mzVOQ-JLmoCJQl14xBuNdtxKrrLLeBcsg4_UTj_uZu2hsRapuHUaSo7JkKP_yMDEkwV72s5CWF24NPHf486GyW21Cu660MQ6rzIR9WE-XL4kg=w588-h461-no?authuser=0"
+    },
+    {
+      list_id: 1,
+      image: "https://lh3.googleusercontent.com/pw/AM-JKLVYkB-B2qjF8O11TeBj7HapRUH7eCKQUjAKmb7qui8gKf-bfoWn4DefTMMvx-9KASw-PLcxfUGxXBS9GBDy9orruzbE7-9bfUNCXNEdewOcEj7PN6W4R9yv1CWUTQ50aCt_9oleRJYfuNVojc1T1VU=w593-h465-no?authuser=0"
+    },
+    # 小原勝幸（おばら かつゆき）
+    {
+      list_id: 2,
+      image: "https://lh3.googleusercontent.com/pw/AM-JKLWIAFwunQMvO7NVEC-AhxYmuDpKyiVf8HX-sx4HCPB_eKF7VcNQ_qpCgApfxO3qCap1nXb4xDtnqQErsNpNTRfY5FtltW4E_baz9vXS0CEg2fhPiCXwo6s-lTWfvglhKnRToHLN6g7FWovgAFYVBgQb=w187-h234-no?authuser=0"
+    },
+    {
+      list_id: 2,
+      image: "https://lh3.googleusercontent.com/pw/AM-JKLXrhKzpbphhhed2LeBvpBjZcdWWizOcG4wv510xvB_4GRVwq7VLooTEjspLuW53GQUpr2d4NENN3mb6Wp8A4FuyGxIrwsgZXTtjRCi-rLF229CUdA4d-rua7k36Xl901pR6rTbd3L6ARaG2sy_MbwmI=w650-h749-no?authuser=0"
+    },
+    # 上地恵栄（うえち けいえい）
+    {
+      list_id: 3,
+      image: "https://lh3.googleusercontent.com/pw/AM-JKLU2YbOTUgOwFJHFyHtCzKYskZLIuhXnaLFjIvAFmVeD-fwathFL-osC_kdalP5hrLKdQI1hKBPuUdwsS2Vpinj7eKSDaby6fvAIH2BOX8iOs_xv89t1LWSLVWO7NemjSYqSLwGaTvNF9LJzTEhFt7B6=w293-h369-no?authuser=0"
+    },
+    {
+      list_id: 3,
+      image: "https://lh3.googleusercontent.com/pw/AM-JKLU3sPMg4P0010YwwXTMoOqyvkmd8kkX6sxh0hHoBTXJW1pe0jtKP-P5UA3esRUeITfXudtw8tDVqSlBZzl_Ks-SU7lVJiGftGZsaKjgHp17g4NU7CowECRYRfS4EzIwijMCj5-4RnypHVJJBa5oK7ja=w400-h566-no?authuser=0"
+    },
+    # 見立真一（みたて しんいち）
+    {
+      list_id: 4,
+      image: "https://lh3.googleusercontent.com/pw/AM-JKLWCo21s6ck3i5UGoZ-HTfRt8FC9a9S0FRGqV7FqmfQ5tYxkLiN_OFv5d1S6ezj-wlO0anidc6ewT2d5QKZnlP68ndkGdhLO-83_6SobE_DvkyYoXVTMRr0fChtUUxuauyql9u7DQYsscr5shu1DQ3BQ=w200-h240-no?authuser=0"
+    },
+    {
+      list_id: 4,
+      image: "https://lh3.googleusercontent.com/pw/AM-JKLUvQPpXwWsvLe-vSxshHMtNnRKdfMbznAkPX-5BD3vYffj5nHHdpxX1rKJR72BXpFiFPYVvcSXJZZig-7mEcHX3bbGSYMLsPrpaY0LSas-fpXsHWa4Gqs_pdtQP7ehuQmQnSSimTI3XCj9pm0_SSxbF=w404-h571-no?authuser=0"
+    },
+    {
+      list_id: 4,
+      image: "https://lh3.googleusercontent.com/pw/AM-JKLWmxIsKEufdAeUOeeiQwl2l4qPGL5Y8Yh3jY_NmuSaiOkBIKM0exYzhZALYS2YS9k3hAL5FRWaO4S33kgyjD7B3P38Dsg3o6y-mPLOFIUarU70mSGQjXbqPGI4N06hgcFPIJAjA8NfVvCOeiBMa19bc=w366-h451-no?authuser=0"
+    },
+    {
+      list_id: 4,
+      image: "https://lh3.googleusercontent.com/pw/AM-JKLXRyCbrmbBSOVZWPqJIkqWsxQIBtzHws6nVKw51CnLzaLbIbZT_pCAHm9fJ9I52Ba2VngJX4QLjh9gATd0NdQYRESs9MWN5VJF_kOOIyczQdTtLUnspfsCUYR0bkScQepKScA-ufrMtP31JR3Zoj8MN=w640-h467-no?authuser=0"
+    },
+    # リン ショウイ
+    {
+      list_id: 5,
+      image: "https://lh3.googleusercontent.com/pw/AM-JKLU15KhQ5UgyxDGyMqwBfw93l9CB2ubUHlXEovS_ky0gzORR1OSVhNIeIQEV1PhS4OEB4IfToR476GKDcraAQUp4MbMlh0b-j_lgVJKrRQmBeSG7a88nlslUf7p7RCdnZZpQ66QAl633_rHjKkqiaBOH=w200-h240-no?authuser=0"
+    },
+    {
+      list_id: 5,
+      image: "https://lh3.googleusercontent.com/pw/AM-JKLUpfiq-hiT0RhfiT8ffzFyE6mIvjgxkmgYOHCnoA0acEBd7zdcrCRHIYcVU14Ze-xPFpHPUjEpKcZGAXYzg25X1jsZiYSqUyCSxY2vpNq7X5ZEcywBv6QsfaGY2Zxg0hWPj_OQRbXU3DUi73O7slZTl=w452-h640-no?authuser=0"
+    },
+    # 吉屋強（よしや つよし）
+    {
+      list_id: 6,
+      image: "https://lh3.googleusercontent.com/pw/AM-JKLW2kMiUdaU_eu_RJemzu9OVzUL9tfYGLAV_2A5xEAxpE1pxyZnyOCF30OoY1v5xlkZGKzvt-ZbyPYMPix-Mesjq2vjKvhuUJNYq7LQPaaZnl6qAJo1skN3ypAqk91V91ZyWjPuQt_hr3HNSsqWXYz0B=w347-h460-no?authuser=0"
+    },
+    {
+      list_id: 6,
+      image: "https://lh3.googleusercontent.com/pw/AM-JKLXITy60LLq6HVSFB-tn7mORcBBNzwHb238p0WPUQ3Bfe85SqNeoXaumG7H16lr-EIy38wai3Tt0PS16sa1u_HuXSYXID-I-YtEu-__7mCClm_-VH9MMe_qCSHKHs1ljZY9HgFfUnUtoA17EzrQo2XmU=w240-h320-no?authuser=0"
+    },
+    {
+      list_id: 6,
+      image: "https://lh3.googleusercontent.com/pw/AM-JKLUzNmTAVEG9GIbVxCp4h5NjzqvZNoamK1dCuD1-E9bIzkHWv8WAMvW7eY9KG8zPy3rG0CmL-uov-sz2X9pMZilX8Vjjxim9aF-PC87BkqVKY7XX3GcwwIMkVyvvsUCTrwsacOVo4VcxeOr0rVYXQ7eO=w1064-h1528-no?authuser=0"
+    },
+    # 宮内雄大（みやうち ゆうだい）
+    {
+      list_id: 7,
+      image: "https://lh3.googleusercontent.com/pw/AM-JKLU0Dl1AipYeEzj9dbBd6dDJrVEF4CjLHdkKWsIswHBSxWH0er9ikV5A40XhW9SDUGVlZYgMa2KzYm6HWf7Tv8G67vhY7IA2gzZk7q4KfRTdSyuGEIWdlu_ShNKsFwfIA6WUpdtzi2k3wbwQkEQqLrUI=w200-h240-no?authuser=0"
+    },
+    {
+      list_id: 7,
+      image: "https://lh3.googleusercontent.com/pw/AM-JKLUdmGbN0_vCI4al9CavB46hz1iws5iQusNbJi3u-pGNNgVF-LWi9mZEqeWHDyvnUXp_FlDAW5LqZ9lkiRhPnHnkEySp1vWnpxZlED2qHM6THEsrgXi678IxwIa1iVDVn-KBQOHG7MyCvGhMuI4k_QCU=w841-h1190-no?authuser=0"
+    },
+    {
+      list_id: 7,
+      image: "https://lh3.googleusercontent.com/pw/AM-JKLV8z0xph4ULK_OiH8kWpOLU86pZAw9gs1YTDvKSOtMvPXm4znPY1mlJecKaHgBOne_WMZizmJ8z8r6qZrii2TzfwsbKgdYlaVbQdPZBCdKxBXuEQocuzaGNMHPJLItLL_3b9yYP-502TlNt0CYsE4fF=w389-h550-no?authuser=0"
+    },
+    {
+      list_id: 7,
+      image: "https://lh3.googleusercontent.com/pw/AM-JKLXffzf9W7NdA8-GTNaGQj3OewPOyHJTdU2jyzW_-yB-tTytfdOT3L1-VUhp7RYZ_hdBdhUCqzFrNaawNtbfGnlS60_K6p7ro84yOO6Whdp6r5e8xgJ_GScJJKN-APajR5u3r7X1DqIZCsTqD6W_ESS4=w448-h252-no?authuser=0"
+    },
+    {
+      list_id: 7,
+      image: "https://lh3.googleusercontent.com/pw/AM-JKLVPAJOY5Eby9z1E6lzErNagxJZr-AC5bnzLD-jpMKrA46k7KeE6xNdWf1CsdbzfOzAZKyUAsKCPH0NigZ4cNXonHHAfKHyzYvweZYUCFZ_-NhFNW9ZbeuMuGaGM4tvIMmAu1XU_WcM0-8iYAhiukMrM=w389-h550-no?authuser=0"
+    },
+    {
+      list_id: 7,
+      image: "https://lh3.googleusercontent.com/pw/AM-JKLWIkcX1v6qy5CIZJoiQ555lk3nx-UxMu52ResCl-5TzL88Qv2YhpkYz5q8wXdwx-EfHimnhnGz3YTW9Rmp4KGG4OXI6UsZKJ-OEFbGdtKLi5jF6KYw1oe-6jGDCXQTrswU6OD5vmOGMPtJmzVXB3131=w389-h550-no?authuser=0"
+    },
+    {
+      list_id: 7,
+      image: "https://lh3.googleusercontent.com/pw/AM-JKLXF0QyZPAqyC8RqBVFfG8RiFFqesazRQ5Ew3Lg8_b3JUOjUzlZuKouer6mQJivcqV2NtLG46QFwmvp5OiBXWzzwAdrqk122AVb6YGerd54oVpVMVj4rTigbBuVJmGLDjUB0SvjqJ4D1KH_2F0mgycAy=w550-h389-no?authuser=0"
+    },
+    {
+      list_id: 7,
+      image: "https://lh3.googleusercontent.com/pw/AM-JKLUV-toXfIiCSwlsRwenhoqQjgpoCsk4NGxHxan4LszBazs0P14BCfdwUDk3kI8T6HqscLfDnH2YM6u5zJfHofOPQ1tayuRxyPmeVQCGnAOArzzU7RQ_-4Tz1yej8e_VN5WIllV6rwRNn-PNd1FMjkXT=w550-h389-no?authuser=0"
+    },
+    {
+      list_id: 7,
+      image: "https://lh3.googleusercontent.com/pw/AM-JKLU3KQZaigPALBznXHX7cRrMHmLup-oOefu5vxfuuUU0TfMpdrlVOuK99p5kN2_MhxpGvo4S2EHPJXUGIzu9_ak-vR5G7e2gGsTFOyrxdWYDa0wgMYtlxKoB_ZpNI4WN1_Cpueq9OJgLiSI1lGAEVBC5=w550-h389-no?authuser=0"
+    },
+    {
+      list_id: 7,
+      image: "https://lh3.googleusercontent.com/pw/AM-JKLVNq5J2yFv-anGroqBo-xizLuukUyYFBenM_uW_LIkbIsyr0Ro9EAuut49nW5gby9nDEqs0B9RnCY-DDV094zo3ypJeSOzGY9KEzYjQHYT_mOzhCQmsZ4ynQd55w8zKtEzl2hukugX3mRYmuV1qN4ZB=w550-h389-no?authuser=0"
+    },
+    {
+      list_id: 7,
+      image: "https://lh3.googleusercontent.com/pw/AM-JKLW9spI_y3m32p9GOVObT_kWs7str3PAjxKH4KSF0K3L8m18WWfQ4eVutzp-MrAzwXlRlrR3Y1lqHvIizKtk_iA8SO4MG9Wbs6kz90N8iQiAB7LRmS8DWGjgNbRfHtSitoiCdrk0bMLk_UuAfOGSpALx=w550-h389-no?authuser=0"
+    },
+    # リ カイ
+    {
+      list_id: 8,
+      image: "https://lh3.googleusercontent.com/pw/AM-JKLWdodsc4sfo9BUxaNgNdTuJNUplHUYd6xd4qZob0Z3cacOYzpwEnv-CNn2ZiiSJLRX_-BnEIcFkKRXgoPScUsMSRZbm3PupSNW0FQatObo9ParBa3e_D7QUERA7HXyA0c23LUyPgiiGHdp_ADtNiqqq=w200-h240-no?authuser=0"
+    },
+    {
+      list_id: 8,
+      image: "https://lh3.googleusercontent.com/pw/AM-JKLXrYfY7jE1zx9rnZpp9q-8Luz7MQRmL0AJ5ETNrGNlFhHp9TrXgCQ2wX_PWFc2DEha8iQnEX2HOjSVgiX5UsI04DC52D-bReT1fbciLxHOpgH8cvN949hliFns5Wx41t-YZqDIX-Z0a5xisx2agLWXA=w240-h320-no?authuser=0"
+    },
+    {
+      list_id: 8,
+      image: "https://lh3.googleusercontent.com/pw/AM-JKLV97QnEePrjgzA_BDQwhVnu0faPB-J7DEgUXhbDrD34SwmBZ3NXTiJw4TvNSDyJ00pf5O6VrKWN8qQjdgeSYlU9bhaWNRNudlogs-nNHLrFQUo5x2VIqynCYtDodqpFgNoCUpecpoDu0b3FwqGr2EVt=w835-h1200-no?authuser=0"
+    },
+    # 越智 清（おち きよし）
+    {
+      list_id: 9,
+      image: "https://lh3.googleusercontent.com/pw/AM-JKLXNK2gHSyLRGiQDu552dy3ee-eBdkTlMhrHhPDfyjtqzZLHpJElny1DBC0BgaIOlr3fbrwPCnt5gv9ZVMUpD4cWK9gxuqTiweIi2ecRMTc2AX7kHx6KgfJDkzm5tVddpxJw7NBUKh00OOG6pKVBFcVz=w200-h240-no?authuser=0"
+    },
+    {
+      list_id: 9,
+      image: "https://lh3.googleusercontent.com/pw/AM-JKLXD7yaR8uk0pTMv_2T7tPByVUFLlklc5aXLMu5jnmur26UoVLG-wYJTVUZAXNhHrr39Ckz7w1Hg-0fA02MV0xblA3HjiljIFUwGL4E4cxYnvfP6HZJip-aE6TCNApGjfo8mN70nUpEgmBXb7NFAwRPY=w150-h157-no?authuser=0"
+    },
+    # 又吉建男（またよし たてお）
+    {
+      list_id: 10,
+      image: "https://lh3.googleusercontent.com/pw/AM-JKLXrIR5fEa7eyvpBzsO93ef9_Zw3LfUSvSd3qMM1He5CZvLTb0MWf71GJoJwzJLzPWYYLBbwYobs3tp2VcJSBr1_oDIZp8hOFeYWyRTIEshXaVr3L0o-srfUeZTjvLhOpdl8HHW9n4dYAYKZaGNutoR3=w200-h240-no?authuser=0"
+    },
+    {
+      list_id: 10,
+      image: "https://lh3.googleusercontent.com/pw/AM-JKLUdPiOi0L9uzzEvG47MfUMFkWYee8frwVQyLZyMEBEi96qJQh6ukkpuNYTGu4WBus78ldIJxCuJ-lsTnHIjZMxbX33sRsa1uIkotmwYcBD1JNksy06tUMA_n4pAGtxDvO7kkqS85acm6OtI7FQ1kmiC=s200-no?authuser=0"
+    },
+    {
+      list_id: 10,
+      image: "https://lh3.googleusercontent.com/pw/AM-JKLVS1ebOxqjeX9KmC3vgyOQb0Oi44saLIyrUfOsCeZvsx9Q3vhJZyrtIh-403zKLgVHGoq_DsRcGT_Y0IYqgp_bGfZaZ4cqq898e27TZwcB21ufjp5qozX9GI1LU-B013luRY9IAeH0eAtAOV17sSjVe=w1098-h1528-no?authuser=0"
+    },
+    # 桐島聡（きりしま さとし）
+    {
+      list_id: 11,
+      image: "https://lh3.googleusercontent.com/pw/AM-JKLXCjhN1MNYdR17D19qe0gcAMRyK-XhKHRwxzRReetXQvofQAWeUPo8ATqKPLQNVBlWh9PbRu1ztemq8_87JqFWRrR96oiqvlTNWgPvMe-vvH93qrbLMAhZ5wbMX-9WzEfQWRQ4YHkdlWEoSRQmPnodS=w200-h240-no?authuser=0"
+    },
+    {
+      list_id: 11,
+      image: "https://lh3.googleusercontent.com/pw/AM-JKLUU3LQ-uiLjhcV4QEFL9lzBy3bVNill-kGqNJhev1RR-9jm_6vGjH17JGkIN9ikdvd8zxtZQTFBNVkVNJheuMcRUoZ8Gvbc8x6hkjZOeQgifeFQZM_bVWzXLlLv0j1sb2bZi6d1f5F2JGMs0uhCksif=w690-h976-no?authuser=0"
+    },
+    # 菱川龍己（ひしかわ たつみ）
+    {
+      list_id: 12,
+      image: "https://lh3.googleusercontent.com/pw/AM-JKLVy8kyz845WKIE3Tee2dWC-RoRelhZLXJTZ6Spd0oeOLCwLJCxyIPwH7tGarzW3eUnbpGK3sEtnb4mYub4c4XBPAiTeswiv2n-83jjTCDxduEtwtb9EWJy5BS0cNyaODWpRjIDGKtv2XmPoAotYYiSE=w849-h1081-no?authuser=0"
+    },
+    {
+      list_id: 12,
+      image: "https://lh3.googleusercontent.com/pw/AM-JKLWZPnHaYbE5PrlUyRfHV3lANQ1m5wXOLc86DwhhgzWKi41CO8cdfncL9-c6c1VyL5xPQFqdh2in4ckwL-EeaAUGd6CTBWSt5VNoBx_u-5efSKlrhPt6p0UKpk6aNsVJURtO5N9sicvwpfhYYSlqWrli=w486-h640-no?authuser=0"
+    },
+    {
+      list_id: 12,
+      image: "https://lh3.googleusercontent.com/pw/AM-JKLV27nHqp8tKWiOj8gG4RUoeMDcJkyiMumLg8awRj5B3o_I8p_d4O2TkmNRd-Et8myCUEjAV3OCzaG_LXFyftS6sLW3n495-_f4YciUUzAhsiX4ZzUoIsnfYK2zDYmK-GTaFflkzBBasmVMPbQnugZ5Y=w417-h600-no?authuser=0"
+    },
+    # 金成行（きん しげゆき）
+    {
+      list_id: 13,
+      image: "https://lh3.googleusercontent.com/pw/AM-JKLWq3VZ_YG1FD1vePKlQRtRQPldcuuwRz66q68omNOTPc1DbIjrf_xUdeM1ZtbpiSXqL7lNY2LaD_2mDhmb182NYS00yg8_XZzafcbive7ZdZD1GVfI_0YP5WmouNhVegJkRfQwECHnHDBJ3RksJ_CwJ=w250-h300-no?authuser=0"
+    },
+    {
+      list_id: 13,
+      image: "https://lh3.googleusercontent.com/pw/AM-JKLXEUokxLHEiGxU2FbJQM_1jRB24GViRVJxGnRtmOnGlX22H4_ZxdwjYT1KVzZODat47Jsaju8ZpdA9aLI4aj_rUkubawunKRp1u27xkg4uc1cWYuz_S7OJFcNRihHh9SHMo1Mx3PGGqsT9qfAcK9hVM=w595-h841-no?authuser=0"
+    },
+    {
+      list_id: 13,
+      image: "https://lh3.googleusercontent.com/pw/AM-JKLU2m7Nw_gjZrWwP2pKCWyvhk6T22O51zE0AtpwRPmA0HNmVHuJMPFNBcRLE-ulf9r_5HoE3KYnpTVI4zYB81mqF3wd9Ay8a4ct4CsYByMwoc3Xs-8-2cEfg--uCmKCBkaOoRyOA3D3nCXw9SuSuGCO6=w1191-h842-no?authuser=0"
     }
   ]
 )
